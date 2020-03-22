af0c0e8d8c04970f0886d1f23460bb82dad4526a
MaksymKarunos <maksim.karunos@gmail.com>
2020-03-22 14:19:42 -0400

    task 1 corrected

--- fig673.c
+++ fig673.c
@@ -1,16 +1,57 @@
-  
-#include <stdio.h>
-a) Assume: char str[5];
- scanf("%s", str); // User types hello
- // This will not work because there is not enough space for the '\0' character
-b) Assume: int a[3];
- printf("$d %d %d\n", a[1], a[2], a[3]);
- // Arrays in C start with index 0 and finish at index length-1, in this case it should have been 0, 1 ,2 
- // $d - is not a valid way to output integers
- // if every line has to be new interger it should have been \n\n\n in the end 
-c) double f[3] = { 1.1, 10.01, 100.001, 1000.0001 };
-// The array is declared to be size 3 but 4 items are intitalized 
-d) Assume: double d[2][10];
- d[1, 9] = 2.345
- // indexing is done using two square brackets like below:
- d[1][9] = 2.345
+// Fig. 6.13: fig06_13.c
+ // Passing arrays and individual array elements to functions.
+ #include <stdio.h>
+ #define SIZE 5
+
+void modifyArray(int b[], size_t size);
+void modifyElement(int e); 
+
+ // function main begins program execution
+ int main(void)
+ {
+ int a[SIZE] = {0, 1, 2, 3, 4}; // initialize array a
+
+ puts("Effects of passing entire array by reference:\n\nThe "
+ "values of the original array are:");
+
+ // output original array
+ for (size_t i = 0; i < SIZE; ++i) {
+     printf("%3d", a[i]);
+ }
+
+puts(""); // outputs a newline
+
+modifyArray(a, SIZE); // pass array a to modifyArray by reference
+ puts("The values of the modified array are:");
+
+ // output modified array
+for (size_t i = 0; i < SIZE; ++i) {
+ printf("%3d", a[i]);
+ }
+
+ // output value of a[3]
+ printf("\n\n\nEffects of passing array element "
+ "by value:\n\nThe value of a[3] is %d\n", a[3]);
+
+modifyElement(a[3]); // pass array element a[3] by value
+
+ // output value of a[3]
+ printf("The value of a[3] is %d\n", a[3]);
+} 
+
+// in function modifyArray, "b" points to the original array "a"
+// in memory
+void modifyArray(int b[], size_t size)
+{
+ // multiply each array element by 2
+ for (size_t j = 0; j < size; ++j) {
+ b[j] *= 2; // actually modifies original array
+ }
+}
+// in function modifyElement, "e" is a local copy of array element
+// a[3] passed from main
+void modifyElement(int e)
+{
+ // multiply parameter by 2
+ printf("Value in modifyElement is %d\n", e *= 2);
+}  
