95b28520e535b710cebc6180b8618a0670c4af88
MaksymKarunos <maksim.karunos@gmail.com>
2020-03-22 12:39:02 -0400

    task 3 bug fix

--- fig830.c
+++ fig830.c
@@ -10,7 +10,7 @@
 
  printf("%s%s\n%s%s\n\n%s%2d\n%s%2d\n%s%2d\n",
  "s1 = ", s1, "s2 = ", s2,
- "memcmp(s1, s2, 4) = ", ,
- "memcmp(s1, s2, 7) = ", ,
-"memcmp(s2, s1, 7) = ", );
+ "memcmp(s1, s2, 4) = ", memcmp(s1, s2, 4),
+ "memcmp(s1, s2, 7) = ", memcmp(s1, s2, 7) ,
+ "memcmp(s2, s1, 7) = ", memcmp(s2, s1, 7));
 } 
