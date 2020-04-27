e716c73ad225a1b53424a4dc4f5f04333f10ff60
MaksymKarunos <maksim.karunos@gmail.com>
2020-04-26 22:53:45 -0400

    final fix

--- HW3/14.5.c
+++ HW3/14.5.c
@@ -2,19 +2,19 @@
 // User-defined Signal Handler 
 #include<stdio.h> 
 #include<signal.h> 
-  
+#include<stdlib.h>
 // Handler for SIGINT, caused by 
 // Ctrl-C at keyboard 
 void handle_sigint(int sig) 
 { 
     printf("Aborting signal %d\n", sig); 
-    exit()
+    exit(0);
 } 
   
 int main() 
 { 
     signal(SIGINT, handle_sigint); 
     while (1) 
-    printf(%s,"Hello World"); 
+    printf("%s","Hello World"); 
     return 0; 
 } 
