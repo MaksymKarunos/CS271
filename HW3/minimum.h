a48d973ad8430f2ce456f82d356de966e9574097
MaksymKarunos <maksim.karunos@gmail.com>
2020-04-26 21:50:07 -0400

    bug fix

--- HW3/minimum.h
+++ HW3/minimum.h
@@ -7,7 +7,7 @@ T minimum( T value1, T value2, T value3 )
    T maximumValue = value1; // assume value1 is maximum
 
    // determine whether value2 is greater than maximumValue
-   if ( value2 < MmaximumValue )
+   if ( value2 < maximumValue )
       maximumValue = value2;
 
    // determine whether value3 is greater than maximumValue
